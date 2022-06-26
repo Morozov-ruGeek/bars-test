a966eca805cbefbabb7ad4991819fbd421da522f
Aleksei Morozov <morozov4811@gmail.com>
2022-06-26 22:39:30 +0400

    fix:0.1.3 modified 01_data.sql and 00_shema.sql

--- .docker/database/init/contract/00_schema.sql
+++ .docker/database/init/contract/00_schema.sql
@@ -3,5 +3,5 @@ create table contracts
     id            serial primary key,
     contract_date timestamp   not null,
     contract_id   varchar(64) not null,
-    updated_at    timestamp default current_timestamp
+    updated_at    timestamp not null
 );
--- .docker/database/init/contract/01_data.sql
+++ .docker/database/init/contract/01_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO contracts(contract_date, contract_id)
-VALUES ('2022-01-19', 'Contract_1'),
-       ('2022-05-19', 'Contract_2'),
-       ('2022-04-19', 'Contract_3');
+INSERT INTO contracts(contract_date, contract_id, updated_at)
+VALUES ('2022-01-19', 'Contract_1', '2022-06-19 10:23:54'),
+       ('2022-05-19', 'Contract_2', '2022-02-19 10:23:54'),
+       ('2022-04-19', 'Contract_3', '2022-03-19 10:23:54');
